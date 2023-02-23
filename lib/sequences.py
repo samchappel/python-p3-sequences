4c6525298287c78e7f18974e9b19596656adbb47
Sam Chappel <m.samchappel@gmail.com>
2023-02-23 07:24:10 -0800

    lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        a, b = 0, 1
+        fib_sequence = [a, b]
+        while len(fib_sequence) < length:
+            a, b = b, a + b
+            fib_sequence.append(b)
+        print(fib_sequence)
